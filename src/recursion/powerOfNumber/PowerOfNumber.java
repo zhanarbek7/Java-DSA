212ae0f067c578f4907ac5772bf2bfe6b0dcf633
zhanarbekabdurasulov <zhanarbek.a7@gmail.com>
2022-12-25 11:40:41 +0600

    power of number in recursion, checked another case

--- src/recursion/powerOfNumber/PowerOfNumber.java
+++ src/recursion/powerOfNumber/PowerOfNumber.java
@@ -2,13 +2,16 @@ package recursion.powerOfNumber;
 
 public class PowerOfNumber {
     public static void main(String[] args) {
-        System.out.println(power(2, 3));
+        System.out.println(power(3, -5));
     }
 
 
     public static int power(int base, int exp){
-        if(exp==0)
-            return 1;
+        if(exp<0){
+            return -1;
+        }
+        if(exp==1)
+            return base;
 
         return base * power(base, exp-1);
     }
